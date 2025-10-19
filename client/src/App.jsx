dbd677eefb97f0c68b871955ae9578eb9fbe6014
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-10-19 17:51:32 +0700

    reafactor: routing in App component to use new ResumeBuilder component and update path structure. Changed route for ResumeBuilderPage to ResumeBuilder with dynamic resume ID parameter.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import ResumeBuilderPage from "./pages/ResumeBuilderPage";
 import Preview from "./pages/Preview";
 import Layout from "./pages/Layout";
+import ResumeBuilder from "./pages/ResumeBuilder";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="app" element={<Layout />}>
-          <Route index path="dashboard" element={<Dashboard />} />
-          <Route path="builder/id:resumeid" element={<ResumeBuilderPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="resume-builder/:resumeid" element={<ResumeBuilder />} />
         </Route>
         <Route path="preview/:reusmeid" element={<Preview />} />
       </Routes>
